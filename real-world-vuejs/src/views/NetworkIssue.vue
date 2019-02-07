91d34e2578ca5a5638bc7d953608dc296fb32d7c
LucasVmigotto <lvmigotto1@gmail.com>
2019-02-07 17:33:41 -0200

    Creating a Nuxt App

--- real-world-vuejs/src/views/NetworkIssue.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Oops!</h1>
-    <h3>Looks like you're experiencing some network issue, please click go back and try again.</h3>
-    <router-link :to="{ name: 'event-list' }">Just take me to home</router-link>
-  </div>
-</template>
